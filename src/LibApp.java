747ad9d707026b914f357a4933ffda7ab045b3fa
Mnli001 <123046550+Mnli001@users.noreply.github.com>
2026-04-07 22:46:37 +0800

    Update LibApp.java

--- src/LibApp.java
+++ src/LibApp.java
@@ -1,11 +1,19 @@
-public class LibApp {
-	public static void main(String[] args) {
-		Library lib = new Library();
-		lib.addBook("Гэдэс", 1921);
-		lib.addBook("1984", 1949);
-		lib.addBook("Сүүн Зам", 2020);
-		for (Book b : lib.getRecentBooks(2000)) {
-			System.out.println(b.getTitle() + ": " + b.getYear());
-		}
-	}
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZooApp {
+    public static void main(String[] args) {
+        Zoo<Mammal> mammalZoo = new Zoo<>();
+        mammalZoo.addAnimal(new Mammal("Арслан"));
+        
+        Zoo<Bird> birdZoo = new Zoo<>();
+        birdZoo.addAnimal(new Bird("Тогоруу"));
+        
+        List<Animal> generalZoo = new ArrayList<>();
+        
+        mammalZoo.transferAnimals(mammalZoo.getAnimals(), generalZoo);
+        birdZoo.transferAnimals(birdZoo.getAnimals(), generalZoo);
+        
+        generalZoo.forEach(a -> System.out.println(a.getName()));
+    }
 }
